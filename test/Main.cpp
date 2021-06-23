c28ed1a76b6a0d963f6b93da4aaf5ce1cf717091
IshiiKouto <takuminn717@gmail.com>
2021-06-23 13:07:08 +0900

    カメラやその他諸々をいじった

--- test/Main.cpp
+++ test/Main.cpp
@@ -23,46 +23,65 @@ void Main()
     TextEditState text1;
     text1.text = U"Enter text";
 
+    Camera2D camera(Vec2(0,0), 1.0, Camera2DParameters::MouseOnly());
+    camera.setCenter(center);
+    camera.setTargetCenter(center);
+
     while (System::Update())
     {
-        const double t = Scene::Time();
-        const double dt = Scene::DeltaTime();
+        {
+            camera.update();
+            const auto trans = camera.createTransformer();
 
-        earth.rotated(t).drawAt(center);
+            const double t = Scene::Time();
+            const double dt = Scene::DeltaTime();
 
-        if (MouseL.down()) {
-            points << Cursor::Pos();
+            earth.rotated(t).drawAt(center);
+
+            if (MouseL.down()) {
+                points << Cursor::Pos();
+            }
+
+            static double baseTheta = 0;
+            baseTheta += dt * 30_deg * sliderValue;
+            for (auto i : step(12)) {
+                const double theta = i * 30_deg + baseTheta;
+                const Vec2 pos = OffsetCircular(center, r, theta);
+                const Vec2 pos2 = OffsetCircular(center, 110, theta * -1);
+                const Vec2 pos3 = OffsetCircular(center, 300, theta * -1);
+
+                texture.flipped(i % 2).rotated(t + i).drawAt(pos);
+                muscle.flipped(i % 2).scaled(0.6).rotated(t + i).drawAt(pos2);
+                dancer.flipped(i % 2).scaled(1.5).rotated(t + i).drawAt(pos3);
+            }
+
+            for (const auto& point : points) {
+                Circle(point, 10).draw(Palette::Aqua);
+            }
+
+            SimpleGUI::TextBox(text1, Vec2(center.x - 200, center.y + 170), 400);
+
+            const String text = text1.text;
+            const size_t length = static_cast<size_t>((int)(t / 0.1) % (text.length() + 1));
+
+            font(text.substr(0, length)).drawAt(center.movedBy(4, 4), Palette::Black);
+            font(text.substr(0, length)).drawAt(center, Palette::Red);
+
+            if (SimpleGUI::Button(U"push here", Vec2(center.x - 50, center.y + 70), 100)) {
+                Scene::SetBackground(RandomColor());
+                Scene::SetLetterbox(RandomColor());
+            }
+            SimpleGUI::Slider(U"{:.2f}"_fmt(sliderValue), sliderValue, -10.0, 10.0, Vec2(center.x - 100, center.y + 120));
         }
 
-        static double baseTheta = 0;
-        baseTheta += dt * 30_deg * sliderValue;
-        for (auto i : step(12)) {
-            const double theta = i * 30_deg + baseTheta;
-            const Vec2 pos = OffsetCircular(center,r,theta);
-            const Vec2 pos2 = OffsetCircular(center, 110, theta*-1);
-            const Vec2 pos3 = OffsetCircular(center, 300, theta*-1);
-
-            texture.flipped(i % 2).rotated(t + i).drawAt(pos);
-            muscle.flipped(i % 2).scaled(0.6).rotated(t + i).drawAt(pos2);
-            dancer.flipped(i % 2).scaled(1.5).rotated(t + i).drawAt(pos3);
+        if (SimpleGUI::Button(U"Reset", Vec2(20, 20)))
+        {
+            // 2D カメラのパラメータをリセット
+            camera.setCenter(center);
+            camera.setTargetCenter(center);
+            camera.setScale(1.0);
+            camera.setTargetScale(1.0);
         }
-
-        for (const auto& point : points) {
-            Circle(point,10).draw(Palette::Aqua);
-        }
-
-        SimpleGUI::TextBox(text1,Vec2(center.x-200,center.y+170),400);
-
-        const String text = text1.text;
-        const size_t length = static_cast<size_t>((int)(t/0.1)%(text.length()+1));
-
-        font(text.substr(0,length)).drawAt(center.movedBy(4,4), Palette::Black);
-        font(text.substr(0,length)).drawAt(center,Palette::Red);
-
-        if (SimpleGUI::Button(U"push here", Vec2(center.x-50,center.y+70),100)) {
-            Scene::SetBackground(RandomColor());
-            Scene::SetLetterbox(RandomColor());
-        }
-        SimpleGUI::Slider(U"{:.2f}"_fmt(sliderValue),sliderValue,-10.0,10.0, Vec2(center.x - 100, center.y + 120));
+        camera.draw(Palette::Orange);
     }
 }
